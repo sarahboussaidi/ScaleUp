bc570105460abb72104e300ef7fd98b33e7de8ed
sarahboussaidi <sarraboussaidi4@gmail.com>
2025-11-06 10:54:59 +0100

    template integration

--- userapp/views.py
+++ userapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def about_view(request):
+    # This will render the shared template page-about.html
+    return render(request, 'page-about.html')
