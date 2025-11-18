95763ceb4887d63e6fc22b58bcec0e5d55654390
islemdrg <islemdarghouth1312@gmail.com>
2025-11-18 20:53:09 +0100

    urls worki

--- worki/urls.py
+++ worki/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('freelanceapp.urls')),
+    
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
